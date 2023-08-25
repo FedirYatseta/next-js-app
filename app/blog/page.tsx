2b77122374b6238fe29a4af014915e9d9f9229d2
Fedir Yatseta <yatseta@gmail.com>
2023-08-25 18:34:20 +0300

    fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,9 @@
 'use client'
 import { getPosts } from '@/api/getPosts'
 import PostSearch from '@/components/PostSearch'
-import { Metadata } from 'next'
 import Link from 'next/link'
 import React from 'react'
 import useSWR from 'swr'
-
-export const metadata: Metadata = {
-    title: 'Blog | NEXT.JS',
-
-}
 
 const Blog = () => {
 
